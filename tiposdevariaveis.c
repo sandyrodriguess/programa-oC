ad77028f7469b73d1b9f718efee9152a9d2aadac
Sandy Rodrigues <130946680+sandyrodriguess@users.noreply.github.com>
2025-09-30 20:04:25 -0300

    Update tiposdevariaveis.c

--- tiposdevariaveis.c
+++ tiposdevariaveis.c
@@ -1,11 +1,12 @@
 #include <stdio.h> 
 
 int main (){
-  int idade; 
-int quantidade;
-float altura;
-char letra;
-char nome[]; 
+  int idade = 25; 
+int quantidade = 1;
+float altura = 1.75;
+  double peso = 90.3;
+char letra = 'A';
+char nome[20] = "mariana" ; 
 
 
 
